6bc2f56e2a51b770343d9945789fd9d46e51fde7
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-31 02:34:23 +0530

    Modified the create course form

--- app/(pages)/course/upload/page.jsx
+++ app/(pages)/course/upload/page.jsx
@@ -35,6 +35,27 @@ const CreateCoursePage = () => {
               </div>
             </div>
 
+            <div className="sm:col-span-4">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-black">
+                Course Price (MATIC)
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <span className="flex select-none items-center pl-3 text-black sm:text-sm">
+
+                  </span>
+                  <input
+                    type="number"
+                    name="price"
+                    id="price"
+                    autoComplete="price"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-black placeholder:text-black focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="1 MATIC"
+                  />
+                </div>
+              </div>
+            </div>
+
             <div className="col-span-full">
               <label htmlFor="about" className="block text-sm font-medium leading-6 text-black">
                 About
@@ -56,7 +77,7 @@ const CreateCoursePage = () => {
 
             <div className="col-span-full">
               <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-black">
-                Cover photo
+                Course Content
               </label>
               <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                 <div className="text-center">
@@ -67,7 +88,7 @@ const CreateCoursePage = () => {
                       className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                     >
                       <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                      <input id="file-upload" name="file-upload" accept="application/pdf" type="file" className="sr-only" />
                     </label>
                     <p className="pl-1">or drag and drop</p>
                   </div>
@@ -75,15 +96,29 @@ const CreateCoursePage = () => {
                 </div>
               </div>
             </div>
+
+            <div className="col-span-full">
+              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-black">
+                Cover Photo
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <PhotoIcon className="mx-auto h-12 w-12 text-black" aria-hidden="true" />
+                  <div className="mt-4 flex text-sm leading-6 text-black">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="image" accept="image/*" className="sr-only" />
+                    </label>
+                    <p className="pl-1">or drag and drop</p>
+                  </div>
+                  <p className="text-xs leading-5 text-black">PNG/JPG/JPEG</p>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-black">Notifications</h2>
-          <p className="mt-1 text-sm leading-6 text-black">
-            We&apos;ll always let you know about important changes, but you pick what else you want to hear about.
-          </p>
-
         </div>
       </div>
 
