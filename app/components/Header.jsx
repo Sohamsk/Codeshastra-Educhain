16945a98a63abb279a5bd6f0e9c12cbb805bc689
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-31 02:14:37 +0530

    Code refactor

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = async ({ username }) => {
               <Link href="/browse" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Browse Courses</Link>
             </li>
             <li>
-              <Link href="/course/create" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Create a Course</Link>
+              <Link href="/course/upload" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Create a Course</Link>
             </li>
             <li>
               <Link href="/course/enrol" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Enrol into Course</Link>
