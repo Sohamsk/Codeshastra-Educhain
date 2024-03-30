78f8253227f57f44fcd586f497e9f7b1ff7d83bd
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-30 16:20:59 +0530

    Installed necessary libraries, added publicRoutes to middleware, setup a proper tailwind config file

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/profile", "/register", "/api/test"],
+  publicRoutes: ["/", "/about", "/browse", "/api/test"],
 });
 
 export const config = {
