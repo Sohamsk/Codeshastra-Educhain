7d426436d59d87048e4f201b68e4cb1a7bc40803
Soham Kudalkar <sohamskudalkar02@gmail.com>
2024-03-30 13:55:32 +0530

    added course model and started uploading routes

--- app/api/courses/upload/route.js
+++ app/api/courses/upload/route.js
@@ -2,5 +2,44 @@ import { connect } from "@/db/connection";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  return NextResponse.json();
+  connect();
+  const {
+    name,
+    id,
+    description,
+    author,
+    author_id,
+    address,
+    price,
+    question,
+    deadline,
+    users,
+    thumbnail,
+    content,
+  } = req.json;
+  if (name == null || undefined) {
+    res.status(400).send("Illegal request");
+  } else {
+    const checkcourse = await Course.findOne({ id: id });
+    if (checkcourse) {
+      console.log("Already in database,id= ", id, " name= ", name);
+      res.status(400).send("Already in database");
+    } else {
+      await Course.create({
+        name: name,
+        id: id,
+        description: description,
+        author: author,
+        author_id: author_id,
+        address: address,
+        price: price,
+        question: question,
+        deadline: deadline,
+        users: users,
+        thumbnail: thumbnail.toString("base64"), // handle file handling
+        content: content, // handle files
+      });
+      NextResponse.json({ result: "Success!" }, { status: 200 });
+    }
+  }
 }
