9334896bf7fb9ab0bbfabd4792fe70f997e243eb
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-30 19:22:16 +0530

    Finished building the Hero component

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,16 @@
-export default function Home() {
+import Hero from "./components/Hero";
+
+const Home = () => {
   return (
     <>
-      <h1 className='text-2xl font-bold mb-5 text-center'>Welcome</h1>
-      <p className='mb-5'>
-        <ul className="text-center">
-          List of Components
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>How it Works</li>
-          <li>Call to Action</li>
-          <li>Footer</li>
-        </ul>
-      </p>
+      <Hero />
+      <li>Pricing</li>
+      <li>How it Works</li>
+      <li>Call to Action</li>
+      <li>Footer</li>
     </>
   );
-}
+};
+
+export default Home;
+
